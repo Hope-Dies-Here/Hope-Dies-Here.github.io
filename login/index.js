77671a19c1fa76bbbfdc4fd7e1758e876eaa8bcb
DNS <onyourleftpal@gmail.com>
2022-07-09 13:15:13 +0300

    new

---
+++ login/index.js
@@ -0,0 +1,52 @@
+
+const u = document.getElementById('username')
+const p = document.getElementById('password')
+const button = document.getElementById('submit')
+
+
+const startingLocation = window.location.pathname
+
+
+
+
+// function updateUsername(d) {
+//     username.value = d
+// }
+
+// function updatePassword(d) {
+//     password.value = d
+// }
+
+ 
+
+
+
+button.addEventListener('click', async () => {
+    username = u.value
+    password = p.value
+    const data = { username, password }
+    
+    const sendThis = {
+        method: 'POST',
+        headers: { 
+        'Content-Type': 'application/json'
+            },
+        body: JSON.stringify(data)
+    }
+
+    const response = await fetch('/login', sendThis)
+    let res = await response.json();
+
+    if(res.status) {
+        console.log('Ur Logged in')
+        window.location = 'next.html'
+        
+    } else{
+        alert('wrong password')
+        console.error('passworrd error')
+    }
+})
+
+
+
+
---
+++ server.js
@@ -0,0 +1,48 @@
+import Express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = Express()
+
+app.use(Express.static(path.join(__dirname, 'server')))
+app.use(Express.json())
+// app.use(logIn)
+
+let auth
+function logIn(req, res, next){
+    if(auth) {
+        next()
+    } else {
+        console.log('err')
+    }
+}
+
+app.post('/login', (req, res) => {
+    console.log(req.body.password)
+
+    if(req.body.password === 'sarah' && req.body.username === 'sarah123') {
+        res.sendFile(__dirname, './server/next.html')
+        res.json({
+            username: req.body.username,
+            password: req.body.password,
+            status: true
+        })
+    } else {
+        res.json({ status: false})
+        console.log('err')
+    }
+
+    console.log(req.body)
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'server', 'index.html'))
+    console.log(req.params)
+})
+
+
+
+app.listen(3000, () => console.log('working'))
