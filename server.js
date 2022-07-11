833d55b307cc06fa05b767b561c09c91d0906d06
DNS <onyourleftpal@gmail.com>
2022-07-11 16:33:23 +0300

    LMAO

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.post('/login', (req, res) => {
     const check = users.find(user => user.name === username)
     const checkP = users.find(user => user.name === password)
 
-    if(check === undefined) {
+    if(check === undefined || checkP === undefined) {
         // res.send('user not found')
         res.sendFile(path.join(__dirname, 'server/next copy.html'))
     } else if(check.name === username && check.password === password) {
