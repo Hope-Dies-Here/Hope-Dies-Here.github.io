8911f1a06f8e2cb8d39e8d7aadca06cf0520658e
DNS <onyourleftpal@gmail.com>
2022-07-12 19:26:54 +0300

    dbv

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get('/goo', secure, (req, res) => {
     }
 })
 
-app.get('/:id', (req, res) => {
+app.get('actual/:id', (req, res) => {
     res.send('bruuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuh :|')
 })
 
